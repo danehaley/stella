b8dea5028dc69b901663bcd71fe86ffa67c5412f
Dane Haley <dane.dobra@fdmgroup.com>
2024-08-12 21:32:16 +1000

    content update

--- public/herlove.js
+++ public/herlove.js
@@ -1,15 +1,25 @@
 // iz blis
+let running = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const jesus = document.getElementById("jesus");
-  if (jesus) {
-    jesus.addEventListener("click", function () {
-      // i love you btw idk if ive told u b4
-      jesus.src = "/gif/stella.gif";
-      setTimeout(function () {
-        jesus.src = "/gif/djJesusNestlesHisLilLamb.gif";
-      }, 2250);
-    });
-  }
+  setJesusClickListener();
   // lay lady lay
 });
+
+function setJesusClickListener() {
+  const jesus = document.getElementById("jesus");
+
+  if (jesus) {
+    jesus.addEventListener("click", function () {
+      if (!running) {
+        running = 1;
+        // i love you btw idk if ive told u b4
+        jesus.src = "/gif/stella.gif";
+        setTimeout(function () {
+          jesus.src = "/gif/djJesusNestlesHisLilLamb.gif";
+          running = 0;
+        }, 2250);
+      }
+    });
+  }
+}
