71c4f42a8ab073252dd138cea8c63f298ae86169
Dane <66982337+danehaley@users.noreply.github.com>
2025-01-09 23:59:50 +1000

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,8 +8,7 @@ function Home() {
         <div className="home-jesus">
           <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
           <p className="home-jesus-quote">
-            somethymes this love shit can be a lil messy, but true love always workz out in da
-            end... §♲
+           happy birthday angel cervine, cemetery doe, baby eterynl... §♲
           </p>
         </div>
       </div>
