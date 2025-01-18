1fa7707861a74afd78d5380fb3afebe8ab2da908
umbel <danehaley22@gmail.com>
2025-01-18 17:40:02 +1000

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,8 +12,7 @@ function Home() {
           <div className="home-jesus">
             <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
             <p className="home-jesus-quote">
-              somethymes this love shit can be a lil messy, but true love always
-              workz out in da end... §♲
+              wow our big numba 1 feedback supplier - wedgieluvr69 - eterynlly grateful for the rose scented feedback, prose matching surely only the prettiest girl in da world... §♲
             </p>
           </div>
         </div>
