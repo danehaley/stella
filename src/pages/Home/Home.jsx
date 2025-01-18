86f0d5d2d00978ad9be724870967f41312fd7f5c
umbel <danehaley22@gmail.com>
2025-01-19 09:33:04 +1000

    less ....

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
           <div className="home-jesus">
             <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
             <p className="home-jesus-quote">
-              im gonna stop for few days.. i love you, i want you, i miss you.. keep working for us.. & 100% dark souls i can hear the screeching from here.. §♲
+              im gonna stop for few days. i love you, i want you, i miss you. keep working for us, & 100% dark souls i can hear the screeching from here.. §♲
             </p>
           </div>
         </div>
