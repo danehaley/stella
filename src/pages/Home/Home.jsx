1ec11303f6d6cc5e94d3c2c1fa163440941c2305
umbel <danehaley22@gmail.com>
2025-01-18 22:34:39 +1000

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
           <div className="home-jesus">
             <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
             <p className="home-jesus-quote">
-              i love you, i want you, sleep tight gypsophila so sweet.. §♲
+              youre so talented... i love you, i want you, sleep tight gypsophila so sweet.. §♲
             </p>
           </div>
         </div>
