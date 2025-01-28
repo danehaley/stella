900f5124fc2910b91173b5cde4202a400b88c5f7
umbel <danehaley22@gmail.com>
2025-01-29 09:03:17 +1000

    logger

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,32 @@
 import "./Home.scss";
+import logger from 'heroku-logger'
 
 function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = () => {
+      if (!isMobile) {
+        logger.info("cm");
+      }
+    };
+
+    const checkMobile = () => {
+      const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      setIsMobile(mobile);
+      if (mobile) {
+        logger.info("pl");
+      }
+    };
+
+    checkMobile();
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [isMobile]);
+
   return (
     <>
       <div className="content-container">
@@ -12,8 +38,9 @@ function Home() {
           <div className="home-jesus">
             <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
             <p className="home-jesus-quote">
-              somethymes this love shit can be a lil messy, but true love always
-              workz out in da end... §♲
+              i don't know what youre going thru, but im sorry if i made it
+              worse. im going to go offline again for both of us. stay safe,
+              keep going, chunks off a glacier.. 
             </p>
           </div>
         </div>
