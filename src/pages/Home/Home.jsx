4962401a41d8706ee8c97e2d7431061449555826
umbel <danehaley22@gmail.com>
2025-01-29 21:29:20 +1000

    centered

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,11 @@ function Home() {
   return (
     <>
       <div className="content-container">
-        <div className="home-wrapper">
-          <div className="home-jesus">
-            <p className="home-jesus-quote">
-            blessing to have known you, to have held a love from you, will never quite forget you. thank you. <br/> caressed a doe, bun, & butterfly freed.
-            </p>
-          </div>
-        </div>
+        <p className="home-jesus-quote">
+          blessing to have known you, to have held a love from you, will never
+          quite forget you. thank you. <br /> caressed a doe, bun, & butterfly
+          freed.
+        </p>
       </div>
     </>
   );
