92b08aaf6b475541b54d4616207f6fc739ec214a
umbel <danehaley22@gmail.com>
2025-01-29 20:49:25 +1000

    last --edit3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <div className="home-wrapper">
           <div className="home-jesus">
             <p className="home-jesus-quote">
-            blessing to have known you, to have held a love from you, will never quite forget you. thank you. <br/> free a doe, bun, and butterfly.
+            blessing to have known you, to have held a love from you, will never quite forget you. thank you. <br/> caressed a doe, bun, & butterfly freed.
             </p>
           </div>
         </div>
