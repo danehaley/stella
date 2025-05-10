66047dbc43fe7d84781f866f91fc1960b71b2416
Dane <66982337+danehaley@users.noreply.github.com>
2025-05-10 19:11:48 +1000

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
             <h4 className="home-jesus-heading underline">PS2 JESUS SAYS</h4>
             <p className="home-jesus-quote">
               somethymes this love shit can be a lil messy, but true love always
-              workz out in da end... closing  . . 12/05/2025 §♲
+              workz out in da end... <br/>closing  . . 12/05/2025 §♲
             </p>
           </div>
         </div>
